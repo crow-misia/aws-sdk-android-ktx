ebca289e55be659741734568f1d0cb70a5316ad8
Zenichi Amano <2736571+crow-misia@users.noreply.github.com>
2023-07-27 11:32:47 +0900

    依存ライブラリ更新
    Signed-off-by: Zenichi Amano <2736571+crow-misia@users.noreply.github.com>

--- buildSrc/src/main/java/Maven.kt
+++ buildSrc/src/main/java/Maven.kt
@@ -1,6 +1,6 @@
 object Maven {
     const val groupId = "io.github.crow-misia.aws-sdk-android-ktx"
-    const val version = "0.28.2"
+    const val version = "0.28.3"
     const val desc = "AWS SDK for Android with Kotlin"
     const val siteUrl = "https://github.com/crow-misia/aws-sdk-android-ktx"
     const val gitUrl = "https://github.com/crow-misia/aws-sdk-android-ktx.git"
