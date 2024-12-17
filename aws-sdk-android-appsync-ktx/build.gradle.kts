9014a6f56327282f625c6e2f25350b1a5c416f72
Zenichi Amano <crow.misia@gmail.com>
2024-12-17 16:41:23 +0900

    build: dokka v2対応
    Signed-off-by: Zenichi Amano <crow.misia@gmail.com>

--- aws-sdk-android-appsync-ktx/build.gradle.kts
+++ aws-sdk-android-appsync-ktx/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.kotlinx.kover)
     alias(libs.plugins.detekt)
     alias(libs.plugins.dokka)
+    alias(libs.plugins.dokka.javadoc)
     id("signing")
     id("maven-publish")
 }
@@ -104,22 +105,10 @@ dependencies {
     androidTestImplementation(libs.truth)
 }
 
-val dokkaJavadoc by tasks.getting(DokkaTask::class) {
-    dokkaSourceSets.named("main") {
-        noAndroidSdkLink = false
-    }
-    dependencies {
-        plugins(libs.dokka.javadoc.plugin)
-    }
-    inputs.dir("src/main/java")
-    outputDirectory = layout.buildDirectory.dir("javadoc").get().asFile
-}
-
-val javadocJar by tasks.registering(Jar::class) {
-    group = JavaBasePlugin.DOCUMENTATION_GROUP
-    description = "Assembles JavaDoc JAR"
-    archiveClassifier = "javadoc"
-    from(dokkaJavadoc.outputDirectory)
+val dokkaJavadocJar by tasks.registering(Jar::class) {
+    description = "A Javadoc JAR containing Dokka Javadoc"
+    from(tasks.dokkaGeneratePublicationJavadoc.flatMap { it.outputDirectory })
+    archiveClassifier.set("javadoc")
 }
 
 publishing {
@@ -139,12 +128,12 @@ publishing {
                 |    Version: $version
             """.trimMargin())
 
-                artifact(javadocJar)
+            artifact(dokkaJavadocJar)
 
-                pom {
-                    name = mavenName
-                    description = Maven.DESCRIPTION
-                    url = Maven.SITE_URL
+            pom {
+                name = mavenName
+                description = Maven.DESCRIPTION
+                url = Maven.SITE_URL
 
                 scm {
                     val scmUrl = "scm:git:${Maven.GIT_URL}"
@@ -197,5 +186,5 @@ detekt {
     buildUponDefaultConfig = true
     allRules = false
     autoCorrect = true
-    config.setFrom(rootDir.resolve("config/detekt.yml"))
+    config.from(rootDir.resolve("config/detekt.yml"))
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,11 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.dokka) apply false
+    alias(libs.plugins.dokka.javadoc) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.plugin.compose) apply false
     alias(libs.plugins.kotlin.plugin.serialization) apply false
     alias(libs.plugins.kotlinx.kover) apply false
-    alias(libs.plugins.detekt) apply false
-    alias(libs.plugins.dokka) apply false
 }
