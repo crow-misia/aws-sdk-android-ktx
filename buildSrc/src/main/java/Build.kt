6f50c4c4071bfa3545d0b03ffcfdbb249ac20c68
Zenichi Amano <crow.misia@gmail.com>
2025-08-14 10:16:57 +0900

    fix!: compile sdk/target sdkを36にアップデート
    Signed-off-by: Zenichi Amano <crow.misia@gmail.com>

--- buildSrc/src/main/java/Build.kt
+++ buildSrc/src/main/java/Build.kt
@@ -1,9 +1,9 @@
 import org.gradle.api.JavaVersion
 
 object Build {
-    const val COMPILE_SDK = 35
+    const val COMPILE_SDK = 36
     const val MIN_SDK = 23
-    const val TARGET_SDK = 35
-    val sourceCompatibility = JavaVersion.VERSION_1_8
-    val targetCompatibility = JavaVersion.VERSION_1_8
+    const val TARGET_SDK = 36
+    val sourceCompatibility = JavaVersion.VERSION_11
+    val targetCompatibility = JavaVersion.VERSION_11
 }
